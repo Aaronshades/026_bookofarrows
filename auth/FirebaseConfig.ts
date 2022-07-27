8c71fe8c9330c6611a23ca0fb480e51989fb8be0
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2022-07-27 19:12:38 +0200

    Using env variables

--- auth/FirebaseConfig.ts
+++ auth/FirebaseConfig.ts
@@ -1,16 +1,16 @@
-import {initializeApp} from "firebase/app";
-import {getFirestore} from "@firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
 
-/*const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_APIKEY,
-	authDomain: process.env.REACT_APP_AUTHDOMAIN,
-	databaseURL: process.env.REACT_APP_databaseURL,
-	projectId: process.env.REACT_APP_projectId,
-	storageBucket: process.env.REACT_APP_storageBucket,
-	messagingSenderId: process.env.REACT_APP_messagingSenderId,
-	appId: process.env.REACT_APP_appId
-};*/
 const firebaseConfig = {
+	apiKey: process.env.NEXT_PUBLIC_APIKEY,
+	authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+	databaseURL: process.env.NEXT_PUBLIC_DATABASEURL,
+	projectId: process.env.NEXT_PUBLIC_PROJECTID,
+	storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+	appId: process.env.NEXT_PUBLIC_APPID
+};
+/*const firebaseConfig = {
 	apiKey: "AIzaSyC0G4XvgWjJwh5_WzRiQMqMGCCNlGQf_6Y",
 	authDomain: "archer-aim-assist.firebaseapp.com",
 	databaseURL: "https://archer-aim-assist-default-rtdb.europe-west1.firebasedatabase.app",
@@ -18,7 +18,7 @@ const firebaseConfig = {
 	storageBucket: "archer-aim-assist.appspot.com",
 	messagingSenderId: "216984383462",
 	appId: "1:216984383462:web:e3df4420aef1fce945b027"
-};
+};*/
 
 export const firebaseApp = initializeApp(firebaseConfig, 'firebaseApp');
 export const database = getFirestore(firebaseApp);
